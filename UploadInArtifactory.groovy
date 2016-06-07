f773d88a83ac95c733f6a525f8988cc057e4de08
bphalak <bhushanph@cybage.com>
2016-06-07 15:48:51 +0530

    Update UploadInArtifactory.groovy

--- UploadInArtifactory.groovy
+++ UploadInArtifactory.groovy
@@ -1,4 +1,4 @@
-File file = new File("/opt/jenkins/workspace/UploadFileArtifact/FileUpload.rtf")
+File file = new File("/opt/jenkins/workspace/all_test_jobs/jenkins_test_jobs/UploadFileArtifact/FileUpload.rtf")
       
 def lines = file.readLines()
      
