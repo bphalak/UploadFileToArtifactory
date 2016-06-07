1e86244cab7f4ba1c44eab72b0c2c3d8b93486f2
bphalak <bhushanph@cybage.com>
2016-06-07 16:57:07 +0530

    Update UploadInArtifactory.groovy

--- UploadInArtifactory.groovy
+++ UploadInArtifactory.groovy
@@ -6,6 +6,12 @@ import hudson.model.*
 def build = Thread.currentThread().executable
 println build.workspace.toString()
 
+File theDir = new File(build.workspace)
+println theDir.exists()
+
+File theDir1 = new File(build.workspace.toString()+"/FileUpload.rtf")
+println theDir1.exists()
+
 File file = new File(build.workspace.toString()+"/FileUpload.rtf")
       
 def lines = file.readLines()
