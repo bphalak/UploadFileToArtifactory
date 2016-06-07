65a3c9ebd30cbe26a8e42154a29f2f7917b62d5e
bphalak <bhushanph@cybage.com>
2016-06-07 16:19:40 +0530

    Update UploadInArtifactory.groovy

--- UploadInArtifactory.groovy
+++ UploadInArtifactory.groovy
@@ -1,7 +1,12 @@
+import jenkins.*
+import jenkins.model.*
+import hudson.*
+import hudson.model.*
+
 def build = Thread.currentThread().executable
 println build.workspace.toString()
 
-File file = new File("FileUpload.rtf")
+File file = new File(build.workspace.toString()+"/FileUpload.rtf")
       
 def lines = file.readLines()
      
